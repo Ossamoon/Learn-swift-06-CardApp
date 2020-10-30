f8b4041d2ace4fb822953854344e54c4ff1b1329
Ossamoon <osamu950623@gmail.com>
2020-10-30 21:37:07 +0900

    書籍の内容に従い完成

---
+++ CardApp/CardApp/CardAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardAppApp.swift
+//  CardApp
+//
+//  Created by 齋藤修 on 2020/10/30.
+//
+
+import SwiftUI
+
+@main
+struct CardAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
